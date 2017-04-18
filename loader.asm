0968407f163b298ad89b7ffdca4e7fee9ae2a554
wangli(李望) <wangli@tencent.com>
2017-04-18 10:21:41 +0800

    增加注释

--- loader.asm
+++ loader.asm
@@ -5,13 +5,13 @@ load_system:
     mov dx, 00000h
     mov cx, 00002h
     mov ax, SYSSEG
-    mov es, ax
+    mov es, ax              ;es:bx 01000h:0h bios读取磁盘写入内存的目标位置
     xor bx, bx
-    mov ax, 0200h+SYSLEN
+    mov ax, 0200h+SYSLEN    ;ah 读扇区功能号2 al读扇区数量 17
     int 013h
     jnc ok_load
     jmp $
 ok_load:
-    jmp 01000h:0
+    jmp 01000h:0            ;读取完成之后跳转
     times 510 - ($-$$) db 0
     dw 0xaa55

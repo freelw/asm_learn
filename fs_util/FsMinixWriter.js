5bfb3201067c5821a7c1265b19a4661610a91cee
李望 <liwang@192.168.9.101>
2017-08-16 21:35:45 +0800

    FsMinixWriter.js added

---
+++ fs_util/FsMinixWriter.js
@@ -0,0 +1,10 @@
+
+function FsMinixWriter(dir) {
+
+}
+
+FsMinixWriter.prototype.getBuffer = function() {
+    return new Buffer(0);
+}
+
+module.exports = FsMinixWriter;
--- fs_util/minix_fs_editor.js
+++ fs_util/minix_fs_editor.js
@@ -3,24 +3,38 @@
 const fs = require('fs');
 const program = require('commander');
 const FsMinixReader = require('./FsMinixReader');
+const FsMinixWriter = require('./FsMinixWriter');
 
 function main() {
     program
         .version('0.0.1')
         .option('-i, --image [value]', 'select minix fs image')
-        .option('-d, --dir [value]', 'select release dir')
+        .option('-d, --dir [value]', 'select release/compress dir')
+        .option('-o, --output [value]', 'output image path')
         .parse(process.argv);
     if (program.image) {
         const image_name = program.image;
-        fs.readFile(image_name, (err, data) => {
-            let fsm = new FsMinixReader(data);
-            console.log(fsm.toString());
-            if (program.dir) {
-                fsm.release(program.dir);
-            } else {
-                console.error('release dir not selected');
-            }
-        });
+        if (program.dir) {
+            fs.readFile(image_name, (err, data) => {        
+                let fsmReader = new FsMinixReader(data);
+                console.log(fsmReader.toString());
+                fsmReader.release(program.dir);
+            });
+        } else {
+            console.error('release dir not selected');
+        }
+    } else if (program.out) {
+        const image_name = program.out;
+        if (program.dir) {
+            let fsmWriter = new FsMinixWriter(dir);
+            fs.writeFile(image_name, fsmWriter.getBuffer(), (err) => {
+                if (err) {
+                    console.error('writeFile error :', err);
+                }
+            });
+        } else {
+            console.error('compress dir not selected');
+        }
     }
 }
 
